ea16b9a8b0f3bf95b0ce21a6d8577bf8bc9db3bb
MarcoApunto <ferreiras.marcoa@gmail.com>
2023-05-15 08:20:45 +0200

    Ejercicio Curso JavaScript Tema02_TiposVars

---
+++ JavaScript/Ejer_Tema02/main.js
@@ -0,0 +1,12 @@
+const dataPersonal = {
+	nombre: "Marco",
+	edad: 28,
+	esDesarrollador: true,
+	nacimiento: new Date(1995, 8, 5),
+	libro: {
+		titulo: "El camino de los reyes",
+		autor: "Brandon Sanderson",
+		anyo: new Date("August 10 2010"),
+		url: "https://www.amazon.es/camino-reyes-Archivo-Tormentas-NOVA/dp/8466657665"
+	}
+};
